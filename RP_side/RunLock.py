3d304a48674d1235ab97613cbaaea2b059248220
rikteem <bhowmickrikteem@gmail.com>
2026-03-31 13:09:37 +0530

    Scan and Monitor Signals generating on two RP boards

--- RP_side/RunLock.py
+++ RP_side/RunLock.py
@@ -3,6 +3,6 @@
 from RP_Lock import *
 import os
 host, port = '192.168.0.99', 5000
-Lock = RP_Server(host, port, 5065, RP_mode = 'scan')
+Lock = RP_Server(host, port, 5065, RP_mode = 'monitor')
 Lock.setup_server(loop=False)
 Lock.start_server()
